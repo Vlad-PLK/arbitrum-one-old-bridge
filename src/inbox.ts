bc8d16d5cb1faf38147da6746a741309a6103a17
Vlad-PLK <leonpolo365@gmail.com>
2025-02-06 17:11:11 +0100

    working and synced version

--- src/inbox.ts
+++ src/inbox.ts
@@ -7,6 +7,7 @@ import {
   InboxMessageDelivered,
   InboxMessageDeliveredFromOrigin,
   DepositEthCall,
+  DepositEthDeprecatedCall,
   BridgeTransfer
 } from "../generated/schema"
 import { BigInt, log } from "@graphprotocol/graph-ts"
@@ -44,9 +45,9 @@ export function handleInboxMessageDeliveredFromOrigin(
 
 export function handleDepositETH(call: ethereum.Call): void {
   log.info("beggining of the call : ", [call.transaction.hash.toHexString(), call.block.number.toString()])
-  if (call.block.number <= BigInt.fromI32(12526508))
-    {
-      log.info("call inside the if statement : ", [call.transaction.hash.toHexString(), call.block.number.toString()])
+  if (call.block.number <= BigInt.fromI32(15447144))
+  {
+    log.info("call inside the if statement : ", [call.transaction.hash.toHexString(), call.block.number.toString()])
       let id = call.transaction.hash.toHex();
       let newBridge = new DepositEthCall(id);
       newBridge.id = id;
@@ -65,5 +66,31 @@ export function handleDepositETH(call: ethereum.Call): void {
       newBridgeTransfer.transferFrom = call.transaction.from;
       newBridgeTransfer.transferTo = call.to
       newBridgeTransfer.save()
-    }
+  }
+}
+
+export function handleDepositETHDeprecated(call: ethereum.Call): void {
+  log.info("beggining of the deprecated call : ", [call.transaction.hash.toHexString(), call.block.number.toString()])
+  if (call.block.number <= BigInt.fromI32(15447144))
+  {
+      log.info("deprecated call inside the if statement : ", [call.transaction.hash.toHexString(), call.block.number.toString()])
+      let id = call.transaction.hash.toHex();
+      let newBridge = new DepositEthDeprecatedCall(id);
+      newBridge.id = id;
+      newBridge.blockNumber = call.block.number;
+      newBridge.transactionHash = call.transaction.hash;
+      newBridge.amount = call.transaction.value;
+      newBridge.save();
+      let newBridgeTransfer = new BridgeTransfer(call.transaction.hash);
+      newBridgeTransfer.amount = newBridge.amount;
+      newBridgeTransfer.blockNumber = newBridge.blockNumber;
+      newBridgeTransfer.from = call.transaction.from;
+      newBridgeTransfer.to = call.to;
+      newBridgeTransfer.hash = call.transaction.hash;
+      newBridgeTransfer.logIndex = 0;
+      newBridgeTransfer.timestamp = call.block.timestamp;
+      newBridgeTransfer.transferFrom = call.transaction.from;
+      newBridgeTransfer.transferTo = call.to
+      newBridgeTransfer.save()
+  }
 }
